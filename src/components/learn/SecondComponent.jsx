8148495e86788ff900de2a71b96db09bfe902caa
huy <lnhuy2024@gmail.com>
2024-08-23 17:41:08 +0700

    video 31

--- src/components/learn/SecondComponent.jsx
+++ src/components/learn/SecondComponent.jsx
@@ -1,11 +1,21 @@
 import "./style.css";
 
 const SecondComponet = () => {
+  const person1 = {
+    name: "le ngoc huy",
+    age: "24",
+  };
+  const nums = [1, 2, 3, 4, 5];
+
   return (
     <>
       <div className="university" style={{ color: "blue" }}>
         HCMIU
       </div>
+      <div>
+        {JSON.stringify(person1)} {person1.age} {nums[1]}
+      </div>
+      <div>{console.log(person1.name)}</div>
       <div className="sid">ITITIU21056</div>
     </>
   );
